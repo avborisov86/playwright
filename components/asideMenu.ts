d9ee4f1a4b497e10fe5e9f535c79bf2bac7b0a66
Антон Борисов <anton.v.borisov@rt.ru>
2024-06-05 20:44:31 +0400

    added allure steps to asideMenu.ts

--- components/asideMenu.ts
+++ components/asideMenu.ts
@@ -1,5 +1,6 @@
 import { BasePage } from '../pages/basePage';
 import {expect} from '@playwright/test';
+import {allure} from "allure-playwright";
 
 export class AsideMenuComponent extends BasePage {
     private readonly selectors = {
@@ -11,12 +12,24 @@ export class AsideMenuComponent extends BasePage {
     }
 
     async clickMenuBtn(name: string): Promise<void> {
-        await this.page.locator(this.selectors.menuBtn(name)).click();
-        await this.page.waitForLoadState('load');
+        await allure.step(`Переходим в меню '${this.selectors.menuBtn(name)}'`, async () => {
+
+            await allure.step(`Нажимаем на кнопку меню '${this.selectors.menuBtn(name)}'`, async () => {
+                await this.page.locator(this.selectors.menuBtn(name)).click();
+            });
+            await allure.step('Ожидаем полной загрузки страницы', async () => {
+                await this.page.waitForLoadState('load');
+            });
+        });
     }
 
     async assertMenuBtnActiveStatus(name: string): Promise<void> {
-        const activeBtnAttr: string = await this.page.locator(this.selectors.menuBtn(name)).getAttribute('aria-selected');
-        await expect(activeBtnAttr).toContain('true');
+        await allure.step(`Проверяем статус кнопки меню '${this.selectors.menuBtn(name)}'`, async () => {
+
+            await allure.step(`Проверяем наличие у кнопки '${this.selectors.menuBtn(name)}' атрибута 'aria-selected'`, async () => {
+                const activeBtnAttr: string = await this.page.locator(this.selectors.menuBtn(name)).getAttribute('aria-selected');
+                await expect(activeBtnAttr).toContain('true');
+            });
+        });
     }
 }
